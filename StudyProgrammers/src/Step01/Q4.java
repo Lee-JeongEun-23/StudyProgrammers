65bffbf6f54cebff74553cc8a8d6ca01e1205aff
jeongeun1223 <jeongeun1223@gmail.com>
2021-09-10 15:04:12 +0900

    Question 4 풀이 (자릿수 더하기)

---
+++ StudyProgrammers/src/Step01/Q4.java
@@ -0,0 +1,25 @@
+package Step01;
+
+public class Q4 {
+	public int solution(int n) {
+        int answer = 0;
+        String arr[] = String.valueOf(n).split("");
+        for(int i = 0; i < arr.length; i++) {
+            answer += Integer.parseInt(arr[i]);
+        }
+        return answer;
+    }
+	
+	//////////////////////////////////////////
+	public int solutionA(int n) {
+        int answer = 0;
+        while(n>0){
+            answer += n%10;
+            if(n<10) {
+            	break;
+            }
+            n=n/10;
+        }
+        return answer;
+    }
+}
