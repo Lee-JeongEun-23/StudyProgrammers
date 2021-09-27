54c6da22245b1df6b9db2a9d23cefa2856d02efa
jeongeun1223 <jeongeun1223@gmail.com>
2021-09-27 15:55:22 +0900

    Question 2 풀이 (가운데 글자 가져오기) > 다른 방식으로 재풀이

--- StudyProgrammers/src/Step01/Q2.java
+++ StudyProgrammers/src/Step01/Q2.java
@@ -17,4 +17,20 @@ public class Q2 {
 		}
         return answer;
     }
+	
+	//해당 문제 두번째 풀었을 때
+	public String solution2nd(String s) {
+        String answer = "";
+        if(s.length()%2 == 0) {
+            answer = s.substring((s.length()/2)-1, (s.length()/2)+1);
+        }else {
+            answer = s.substring((s.length()/2), (s.length()/2)+1);
+        }
+        return answer;
+    }
+	
+	//////////////////////////////////////////
+	String solutionA(String word){
+        return word.substring((word.length()-1) / 2, word.length()/2 + 1);    
+    }
 }
