39c8eb2e767b96cf131b93401f0909b5ee1066c9
jeongeun1223 <jeongeun1223@gmail.com>
2021-10-03 11:25:32 +0900

    Question 21 풀이 (짝수와 홀수)

--- StudyProgrammers/src/Step01/Q21.java
+++ StudyProgrammers/src/Step01/Q21.java
@@ -6,7 +6,7 @@ public class Q21 {
 	public String solution(int num) {
         String answer = (num == 0 || num % 2 == 0) ? "Even" : "Odd";
         return answer;
-    }	
+    }
 	/*
 	 0 % x = 0 이므로
 	  위의 코드를 더 단순화 하면,
