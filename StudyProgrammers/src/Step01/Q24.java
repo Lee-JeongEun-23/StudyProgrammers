a94cd4d9f7f67b0540b6e9b482227840f0ed090c
jeongeun1223 <jeongeun1223@gmail.com>
2021-10-07 11:04:37 +0900

    Question 24 풀이 (정수 제곱근 판별) > 해결 완료

--- StudyProgrammers/src/Step01/Q24.java
+++ StudyProgrammers/src/Step01/Q24.java
@@ -1,13 +1,24 @@
 package Step01;
 
+import java.util.regex.Pattern;
+
 public class Q24 {
 	
 	//정수 제곱근 판별
-	public long solution(long n) { //틀린 이유 찾기
-		long answer = -1;
-        if(n > 0 && String.valueOf(Math.sqrt(n)).contains(".0")) {
-        answer = (long) Math.pow((Math.sqrt(n) + 1), 2);
+	 public long solution(long n) {
+        long answer = -1;
+        String pattern = "^[0-9]*.0$";
+        if(n > 0 && Pattern.matches(pattern, String.valueOf(Math.sqrt(n)))) {   
+            answer = (long) Math.pow((Math.sqrt(n) + 1), 2);
         }
         return answer;
     }
+	 
+ 	//////////////////////////////////////////
+	 public long solutionA(long n) {
+	      if (Math.pow((int)Math.sqrt(n), 2) == n) {
+	        return (long) Math.pow(Math.sqrt(n) + 1, 2);
+	      }
+	      return -1;
+	  }
 }
