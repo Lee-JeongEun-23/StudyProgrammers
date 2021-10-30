263d521b3d42c83539872f72ba84157b2cdaa04f
jeongeun1223 <jeongeun1223@gmail.com>
2021-10-30 13:22:15 +0900

    Question 34 풀이 (완주하지 못한 선수)

---
+++ StudyProgrammers/src/Step01/Q34.java
@@ -0,0 +1,53 @@
+package Step01;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Q34 {
+	
+	//완주하지 못한 선수
+	public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for(int i = 0; i < participant.length-1; i++) {
+            if(!participant[i].equals(completion[i])) {
+                answer = participant[i];
+                break;
+            }
+        }
+        if(answer.equals("")) {
+            answer = participant[participant.length-1];
+        }
+        return answer;
+    }
+
+	//////////////////////////////////////////	
+	public String solutionA(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
+            }
+        }
+        return answer;
+    }
+	
+	public String solutionB(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
+        for ( i=0; i<completion.length; i++){
+
+            if (!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[i];
+    }
+}
