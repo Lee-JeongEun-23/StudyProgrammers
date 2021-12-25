f74af920b862db6aa6a4804e73940077fb90c33b
Lee-JeongEun-23 <jeongeun1223@gmail.com>
2021-12-25 23:19:00 +0900

    Question 6 풀이 (기능개발) 2차시도 : 63.6/100

--- StudyProgrammers/src/Step02/Q6.java
+++ StudyProgrammers/src/Step02/Q6.java
@@ -9,6 +9,34 @@ public class Q6 {
 	    for(int i = 0; i < progresses.length; i++) {
 	    	temp[i] = (100 - progresses[i]) % speeds[i] == 0? (100 - progresses[i]) / speeds[i] : ((100 - progresses[i]) / speeds[i])+1;
 	    }
+	    
+	    int stop = temp[0];
+	    int len = 0;
+	    for(int i = 0; i < temp.length; i++) {
+	    	if(stop > temp[i]) { 
+	    		progresses[i] = -1;
+	    	}else {
+	    		stop = temp[i];
+	    		progresses[i] = stop;
+	    		len ++;
+	    	}
+	    }
+	    int count = 1;
+	    answer = new int[len];
+	    int j = 0;
+	    for(int i = 0; i < progresses.length; i++) {
+	    	if(progresses[i] < 0) {
+	    		count ++;
+	    		answer[j] = count;
+	    	}else {
+	    		count = 1;
+	    		if(i != 0) {
+	    			j ++;
+	    		}
+		    	answer[j] = count;
+	    	}
+	    }
+	    /*
 	    int count = 0;
 	    int stop = temp[0];
 	    String ans = "";
@@ -30,6 +58,7 @@ public class Q6 {
 	    for(int i = 0; i < val.length; i++) {
 	    	answer[i] = Integer.parseInt(val[i]);
 	    }
+	    */
         return answer;
     }
 }
