74827e2340b1892b4ddba23f885320d1ef33ff28
Lee-JeongEun-23 <jeongeun1223@gmail.com>
2022-02-02 21:44:04 +0900

    Question 8 풀이 (메뉴 리뉴얼) 80/100

--- StudyProgrammers/src/Step02/Q8.java
+++ StudyProgrammers/src/Step02/Q8.java
@@ -2,39 +2,13 @@ package Step02;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 
-public class Q8 {
-	static void combination(String[] order, boolean[] visited, int start, int n, int r, HashMap<String,Integer> count, ArrayList<String> count2) {
-		if (r == 0) {
-			String temp = "";
-			for (int i = 0; i < n; i++) {
-	            if (visited[i]) {
-	               temp += order[i];
-	            }
-	        }
-			if(count.containsKey(temp)) {
-				count.put(temp, count.get(temp)+1);
-			}else {
-				count2.add(temp);
-				count.put(temp, 1);
-			}
-            return;
-        }
-
-        for (int i = start; i < n; i++) {
-            visited[i] = true;
-            combination(order, visited, i + 1, n, r - 1, count, count2);
-            visited[i] = false;
-        }
-    }
-    
-    public static void main(String[] args) {
-
-        String[] orders = {"XYZ", "XWY", "WXA"};
-        int[] course = {2,3,4};
-
-        HashMap<String, Integer> count = new HashMap<String,Integer>();
+class Q8 {
+    public String[] solution(String[] orders, int[] course) {
+        String[] answer = {};
+         HashMap<String, Integer> count = new HashMap<String,Integer>();
         HashMap<Integer, String> temp = new HashMap<Integer, String>();
         ArrayList<String> count2 = new ArrayList<String>();
         for(int i = 0; i < orders.length; i++) {
@@ -74,8 +48,48 @@ public class Q8 {
             	}
         	}      	
         }
-        System.out.println(count);
-        System.out.println(temp);
+        ArrayList<String> result = new ArrayList<String>();
+        for(int i = course[0]; i <= course[course.length-1]; i++) {
+        	if(temp.containsKey(i)) {
+        		if(temp.get(i).indexOf(",") > 0) {
+        			String[] temp3 = temp.get(i).split(",");
+        			for(int j = 0; j < temp3.length; j++) {
+        				result.add(temp3[j]);
+        			}
+        		}else {
+        			result.add(temp.get(i));
+        		}
+        	}	
+        }
+        Collections.sort(result);
+        answer = new String[result.size()];
+        for(int i = 0; i < result.size(); i++) {
+        	answer[i] = result.get(i);
+        }
+        return answer;
+    }
+    
+    public void combination(String[] order, boolean[] visited, int start, int n, int r, HashMap<String,Integer> count, ArrayList<String> count2) {
+		if (r == 0) {
+			String temp = "";
+			for (int i = 0; i < n; i++) {
+	            if (visited[i]) {
+	               temp += order[i];
+	            }
+	        }
+			if(count.containsKey(temp)) {
+				count.put(temp, count.get(temp)+1);
+			}else {
+				count2.add(temp);
+				count.put(temp, 1);
+			}
+            return;
+        }
 
+        for (int i = start; i < n; i++) {
+            visited[i] = true;
+            combination(order, visited, i + 1, n, r - 1, count, count2);
+            visited[i] = false;
+        }
     }
 }
