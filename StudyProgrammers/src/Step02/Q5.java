371a54d3c2550fead2f1ba90dceb076a2878b013
jeongeun1223 <jeongeun1223@gmail.com>
2022-03-25 12:29:16 +0900

    Question 5 풀이 (전화번호 목록) 완료

--- StudyProgrammers/src/Step02/Q5.java
+++ StudyProgrammers/src/Step02/Q5.java
@@ -1,25 +1,18 @@
 package Step02;
 
+import java.util.Arrays;
+
 public class Q5 {
 	
-	//전화번호 목록 (83.3 / 100)
-	//문제는 전부 다 맞았으나 타당성 전부 시간초과ㅠㅠ
+	//전화번호 목록
 	public boolean solution(String[] phone_book) {
         boolean answer = true;
+        Arrays.sort(phone_book);
+
         for(int i = 0; i < phone_book.length-1; i++){
-            for(int j = i+1; j < phone_book.length; j++) {
-            	if(phone_book[i].length() <= phone_book[j].length()) {
-            		if(phone_book[j].substring(0,phone_book[i].length()).equals(phone_book[i])) {
-                		answer = false;
-                		break;
-            		}
-            	}
-            	if(phone_book[i].length() >= phone_book[j].length()) {
-            		if(phone_book[i].substring(0,phone_book[j].length()).equals(phone_book[j])) {
-                		answer = false;
-                		break;
-            		}
-            	}
+            if(phone_book[i+1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
             }
         }
         return answer;
